9529951653914b7b8f040627ce37b060a719a0fc
Assassin88 <5540832@mail.ru>
2017-03-24 00:44:28 +0300

    My backup
    My backup

---
+++ Backup/Backup/Backup/Classes/Flash.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Backup.Classes
+{
+	class Flash : Storage
+	{
+        double v_memory;
+        double speed_usb = 119;
+        double free_memory;
+
+        public Flash(string Name_, string Model_, double V_memory_)
+            : base(Name_, Model_)
+        {
+            V_memory = V_memory_;
+        }
+
+        public double V_memory
+		{
+			get
+			{
+				return v_memory;
+			}
+
+			set
+			{
+				if (value > 0) { v_memory = value; }	
+			}
+		}
+
+		public double Speed_usb
+		{
+			get
+			{
+				return speed_usb;
+			}
+
+			set
+			{
+				if (value > 0) { speed_usb = value; }
+			}
+		}
+
+		public double Free_memory
+		{
+			get
+			{
+				return free_memory;
+			}
+
+			set
+			{
+				if (value >= 0) { free_memory = value; }
+			}
+		}
+
+		public override double Get_Free_Mem()
+		{
+			return free_memory;
+		}
+
+        public override double Get_Speed_rec()
+        {
+            return V_memory / Speed_usb;
+        }
+
+		public override void Copy_Data(ref double V)	
+		{
+            double local = 0; int count = 0;
+			while (local <= V_memory)
+			{
+				if (local + File_Memory > V_memory) break;
+				{
+					V -= File_Memory;
+					local += File_Memory;
+					++count;
+				}
+			}
+			Free_memory = V_memory - local;
+		}
+
+		public override double Get_Memory()
+		{
+			return v_memory;
+		}
+
+		public override string ToString()
+		{
+			return String.Format("{0}\t{1}\t{2}\t{3}", Name, Model, V_memory, Speed_usb);
+		}
+	}
+}
---
+++ Backup/Backup/Backup/Classes/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Backup.Classes;
+
+namespace Backup
+{
+	class Program
+	{
+        static void Main(string[] args)
+		{
+            double V_Mem = 565000;
+
+            //Storage[] GB = {
+            //    new Flash("USB_3.0", "Flop", 32000),
+            //    new Flash("USB_3.0", "Flop", 8000),
+            //    new Flash("USB_3.0", "Flop", 16000),
+            //    new Flash("USB_3.0", "Flop", 32000),
+            //    new Flash("USB_3.0", "Flop", 16000),
+            //    new Flash("USB_3.0", "Flop", 64000)
+            //    };
+
+            //Storage[] GB = {
+            //    new DVD("DVD", "RW", 1),
+            //    new DVD("DVD", "RW", 1),
+            //    new DVD("DVD", "RW", 1),
+            //    new DVD("DVD", "RW", 2),
+            //    new DVD("DVD", "RW", 2),
+            //    new DVD("DVD", "RW", 2)
+            //    };
+
+
+            //Storage[] GB = {
+            //    new HDD("HDD", "2.0", 120000),
+            //    new HDD("HDD", "2.0", 120000),
+            //    new HDD("HDD", "2.0", 120000),
+            //    new HDD("HDD", "2.0", 60000),
+            //    new HDD("HDD", "2.0", 60000),
+            //    new HDD("HDD", "2.0", 20000)
+            //    };
+
+
+            Storage[] GB = {
+                new Flash("USB", "3.0", 32000),
+                new Flash("USB", "3.0", 64000),
+                new DVD("DVD", "RW", 1),
+                new DVD("DVD", "RW", 2),
+                new HDD("HDD", "2.0", 240000),
+                new HDD("HDD", "2.0", 120000)
+                };
+
+            Console.WriteLine("Fuul_information_memories_in_STORAGE!!!\n");
+            Information.Type_Invorm(GB);
+            Console.WriteLine(); Console.WriteLine();
+
+            Console.WriteLine("Calculation_information_by_STORAGE!!!\n");
+            Information.All_Inform(GB, ref V_Mem);
+            Console.WriteLine();
+            Console.Read();
+		}
+	}
+}
